9f14bf503de78bed5c885f4edbc0a8fc956efec6
kahgeh.tan <kahgeh.tan@xero.com>
2021-08-29 22:13:59 +1000

    enable prettier and eslint for typescriptreact

---
+++ ftplugin/typescriptreact.lua
@@ -0,0 +1,14 @@
+
+lvim.lang.typescriptreact.formatters = {
+  {
+    exe = "prettier",
+    args = {},
+  },
+}
+
+lvim.lang.typescriptreact.linters = {
+  {
+    exe = "eslint_d",
+    args = {},
+  },
+}
