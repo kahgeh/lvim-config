b4f0a7d343290c3ff1dc988855d53264c09e2936
kahgeh <kahgeh@hotmail.com>
2021-11-13 20:56:08 +1100

    format config.lua

--- config.lua
+++ config.lua
@@ -18,32 +18,32 @@ lvim.keys.normal_mode["<A-d>"] = "daw"
 lvim.keys.normal_mode["<C-a>"] = "^"
 lvim.keys.normal_mode["<C-e>"] = "$"
 lvim.keys.normal_mode["<A-Right>"] = "w"
-vim.api.nvim_set_keymap('n', 'd', '"_d', {noremap = true})
-vim.api.nvim_set_keymap('v', 'd', '"_d', {noremap = true})
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true })
+vim.api.nvim_set_keymap("v", "d", '"_d', { noremap = true })
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects" }
-lvim.builtin.which_key.mappings.E ={ "<cmd>TroubleToggle<cr>", "Error/Warnings" }
+lvim.builtin.which_key.mappings.E = { "<cmd>TroubleToggle<cr>", "Error/Warnings" }
 lvim.builtin.which_key.mappings.R = {
-  name= "+Rust",
-  v = { ":lua require('ls-crates').insert_latest_version()<cr>", "insert latest crate version" }
+	name = "+Rust",
+	v = { ":lua require('ls-crates').insert_latest_version()<cr>", "insert latest crate version" },
 }
-lvim.builtin.which_key.mappings.F ={"<cmd>setlocal foldmethod=syntax<cr>", "fold based on syntax"}
+lvim.builtin.which_key.mappings.F = { "<cmd>setlocal foldmethod=syntax<cr>", "fold based on syntax" }
 lvim.builtin.which_key.mappings.t = {
-  name="+Terminal",
-  b = {"<cmd>ToggleTerm direction=horizontal size=10<cr>", "bottom terminal"},
-  r = {"<cmd>ToggleTerm direction=vertical size=85<cr>", "right terminal"},
-  w = {"<cmd>vsp +term<cr>", "right terminal"}
+	name = "+Terminal",
+	b = { "<cmd>ToggleTerm direction=horizontal size=10<cr>", "bottom terminal" },
+	r = { "<cmd>ToggleTerm direction=vertical size=85<cr>", "right terminal" },
+	w = { "<cmd>vsp +term<cr>", "right terminal" },
 }
-lvim.builtin.which_key.mappings.g.t={":luafile ~/.github-gist-secret.lua<cr>", "load token"}
-lvim.builtin.which_key.mappings.g.S={":Gist<cr>", "save gist"}
-lvim.builtin.which_key.mappings.l.h={"<cmd>lua vim.lsp.buf.hover()<cr>", "help"}
-lvim.builtin.which_key.mappings.S = require('user.surround').generate_whichkey_bindings()
+lvim.builtin.which_key.mappings.g.t = { ":luafile ~/.github-gist-secret.lua<cr>", "load token" }
+lvim.builtin.which_key.mappings.g.S = { ":Gist<cr>", "save gist" }
+lvim.builtin.which_key.mappings.l.h = { "<cmd>lua vim.lsp.buf.hover()<cr>", "help" }
+lvim.builtin.which_key.mappings.S = require("user.surround").generate_whichkey_bindings()
 
-local replace_kb = require( 'user.replace' ).generate_whichkey_bindings()
+local replace_kb = require("user.replace").generate_whichkey_bindings()
 lvim.builtin.which_key.mappings.G = {
-  name="+Global",
-  s = replace_kb.global_search
+	name = "+Global",
+	s = replace_kb.global_search,
 }
 lvim.builtin.which_key.vmappings.r = replace_kb.replace
 
@@ -53,9 +53,9 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.side = "left"
 lvim.builtin.dap.active = true
-lvim.builtin.dap.on_config_done = function (_)
-  lvim.builtin.which_key.mappings.d.O = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" }
-  lvim.builtin.which_key.mappings.d.u = { "<cmd>lua require'dapui'.toggle()<cr>", "UI" }
+lvim.builtin.dap.on_config_done = function(_)
+	lvim.builtin.which_key.mappings.d.O = { "<cmd>lua require'dap'.step_out()<cr>", "Step Out" }
+	lvim.builtin.which_key.mappings.d.u = { "<cmd>lua require'dapui'.toggle()<cr>", "UI" }
 end
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = "maintained"
@@ -77,111 +77,111 @@ lvim.builtin.bufferline.active = true
 --   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 -- end
 lvim.lsp.automatic_servers_installation = true
-vim.list_extend(lvim.lsp.override, {"powershell_es", "tailwindcss", "ember"})
-require'lspconfig'.powershell_es.setup{
-  bundle_path= "/Users/kahgeh.tan/bin/pwsh_es"
-}
+vim.list_extend(lvim.lsp.override, { "powershell_es", "tailwindcss", "ember" })
+require("lspconfig").powershell_es.setup({
+	bundle_path = "/Users/kahgeh.tan/bin/pwsh_es",
+})
 
 -- Additional Plugins
-lvim.plugins = { 
-  { "lunarvim/colorschemes" },
-  {
-    "kahgeh/ls-crates.nvim",
-    config= function()
-      require "ls-crates"
-    end,
-    ft={'toml'}
-  },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
-  {
-    "folke/zen-mode.nvim",
-  },
-  {
-    "unblevable/quick-scope",
-    config = function()
-      require "user.quickscope"
-    end,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    -- event = "BufReadPre",
-    config = function()
-      require "user.blankline"
-    end,
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    event = "InsertEnter",
-    config = function()
-      require('nvim-ts-autotag').setup()
-    end
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    run = "cd app && npm install",
-    ft = "markdown",
-  },
-  {
-    "Pocco81/AutoSave.nvim",
-    config = function()
-      require('autoSave').setup({
-        enabled = true,
-        execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
-        events = {"InsertLeave", "TextChanged"},
-        conditions = {
-            exists = true,
-            filetype_is_not = {},
-            modifiable = true
-        },
-        write_all_buffers = false,
-        on_off_commands = true,
-        clean_command_line_interval = 0,
-        debounce_delay = 60000
-    })
-    end
-  },
-  {
-    "mattn/webapi-vim"
-  },
-  {
-    "mattn/vim-gist",
-    event = "BufRead",
-    requires = "mattn/webapi-vim",
-    config = function ()
-      require "user.gist"
-    end
-  },
-  {
-    "tpope/vim-surround",
-    event = "BufRead",
-    keys = {"c", "d", "y"}
-  },
-  {
-    "phaazon/hop.nvim",
-    event = "BufRead",
-    config = function()
-      require("user.hop").config()
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    requires = {"mfussenegger/nvim-dap"},
-    config = function()
-      require('dapui').setup()
-    end
-  }
+lvim.plugins = {
+	{ "lunarvim/colorschemes" },
+	{
+		"kahgeh/ls-crates.nvim",
+		config = function()
+			require("ls-crates")
+		end,
+		ft = { "toml" },
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	{
+		"folke/zen-mode.nvim",
+	},
+	{
+		"unblevable/quick-scope",
+		config = function()
+			require("user.quickscope")
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		-- event = "BufReadPre",
+		config = function()
+			require("user.blankline")
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		ft = "markdown",
+	},
+	{
+		"Pocco81/AutoSave.nvim",
+		config = function()
+			require("autoSave").setup({
+				enabled = true,
+				execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+				events = { "InsertLeave", "TextChanged" },
+				conditions = {
+					exists = true,
+					filetype_is_not = {},
+					modifiable = true,
+				},
+				write_all_buffers = false,
+				on_off_commands = true,
+				clean_command_line_interval = 0,
+				debounce_delay = 60000,
+			})
+		end,
+	},
+	{
+		"mattn/webapi-vim",
+	},
+	{
+		"mattn/vim-gist",
+		event = "BufRead",
+		requires = "mattn/webapi-vim",
+		config = function()
+			require("user.gist")
+		end,
+	},
+	{
+		"tpope/vim-surround",
+		event = "BufRead",
+		keys = { "c", "d", "y" },
+	},
+	{
+		"phaazon/hop.nvim",
+		event = "BufRead",
+		config = function()
+			require("user.hop").config()
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		requires = { "mfussenegger/nvim-dap" },
+		config = function()
+			require("dapui").setup()
+		end,
+	},
 }
 
 require("user.dap").config()
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
-  { "FileType", "rust", "set matchpairs+=<:>" },
-  { "FileType", "csharp", "set matchpairs+=<:>" },
-  { "FileType", "typescript", "set matchpairs+=<:>"}
+	{ "FileType", "rust", "set matchpairs+=<:>" },
+	{ "FileType", "csharp", "set matchpairs+=<:>" },
+	{ "FileType", "typescript", "set matchpairs+=<:>" },
 }
 
 vim.cmd(":set background=dark")
