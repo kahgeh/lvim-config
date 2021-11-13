0e8318414f0bac8a45d06d1685f166e5c551a453
kahgeh <kahgeh@hotmail.com>
2021-11-13 21:05:58 +1100

    add eslint

--- ftplugin/typescript.lua
+++ ftplugin/typescript.lua
@@ -1,9 +1,11 @@
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    exe = "prettier",
-    filetypes = { "typescript" },
-    args = {},
-  },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{
+		exe = "prettier",
+		filetypes = { "typescript" },
+		args = {},
+	},
+})
 
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({ { exe = "eslint_d", filetypes = { "typescript" } } })
--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -1,9 +1,10 @@
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    exe = "prettier",
-    filetypes = { "typescriptreact" },
-    args = {},
-  },
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{
+		exe = "prettier",
+		filetypes = { "typescriptreact" },
+		args = {},
+	},
+})
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({ { exe = "eslint_d", filetypes = { "typescriptreact" } } })
