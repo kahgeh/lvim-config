c8acc5b81a6eb8db4f0cae05233696477c32d22b
kahgeh <kahgeh@hotmail.com>
2021-11-19 06:29:12 +1100

    use number for harpoon term to avoid interfering with to

--- lua/user/keybinding.lua
+++ lua/user/keybinding.lua
@@ -2,10 +2,10 @@ local M = {}
 
 local function set_harpoon_keymaps()
 	lvim.keys.normal_mode["<C-Space>"] = "<cmd>lua require('harpoon.cmd-ui').toggle_quick_menu()<CR>"
-	lvim.keys.normal_mode["tu"] = "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>"
-	lvim.keys.normal_mode["te"] = "<cmd>lua require('harpoon.term').gotoTerminal(2)<CR>"
-	lvim.keys.normal_mode["cu"] = "<cmd>lua require('harpoon.term').sendCommand(1, 1)<CR>"
-	lvim.keys.normal_mode["ce"] = "<cmd>lua require('harpoon.term').sendCommand(1, 2)<CR>"
+	lvim.keys.normal_mode["t4"] = "<cmd>lua require('harpoon.term').gotoTerminal(1)<CR>"
+	lvim.keys.normal_mode["t5"] = "<cmd>lua require('harpoon.term').gotoTerminal(2)<CR>"
+	lvim.keys.normal_mode["c4"] = "<cmd>lua require('harpoon.term').sendCommand(1, 1)<CR>"
+	lvim.keys.normal_mode["c5"] = "<cmd>lua require('harpoon.term').sendCommand(1, 2)<CR>"
 	lvim.builtin.which_key.mappings["a"] = { "<cmd>lua require('harpoon.mark').add_file()<CR>", "Add Mark" }
 	lvim.builtin.which_key.mappings["<leader>"] = {
 		"<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
